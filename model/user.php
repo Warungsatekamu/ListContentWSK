9f37cb4dd5e64d90c74ce39a5e371d8ecaed881a
pierresebastian <pierresebastian30@gmail.com>
2022-07-18 12:12:12 +0700

    update generate password

--- model/user.php
+++ model/user.php
@@ -45,10 +45,10 @@
         }
 
 		public function randomPassword() {
-			$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+			$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%^&*(),.<>/?;[]}{";
 			$pass = array(); //remember to declare $pass as an array
 			$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-			for ($i = 0; $i < 12; $i++) {
+			for ($i = 0; $i < 60; $i++) {
 				$n = rand(0, $alphaLength);
 				$pass[] = $alphabet[$n];
 			}
